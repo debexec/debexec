d4b87a263d474597e7af3a18a2e2c620ba13e523
Erich E. Hoover <erich.e.hoover@gmail.com>
2022-04-26 17:53:33 -0600

    make the core utility installer better at finding packages

--- install-coreutils.sh
+++ install-coreutils.sh
@@ -2,14 +2,26 @@
 mkdir -p /var/lib/dpkg/{info,updates}
 touch /var/lib/dpkg/status
 
+find_pattern() {
+    DEBS=""
+    for DEB in "$@"; do
+        if [[ -f "${DEBPATH}"/${DEB}_*.deb ]]; then
+            DEB=$(cd "${DEBPATH}"; ls ${DEB}_*.deb | sed 's/^\([^_]*\)_.*/\1/')
+        fi
+        DEBS="${DEBS} ${DEB}"
+    done
+    echo "${DEBS}"
+}
+
 # install the dependencies required by coreutils, dpkg, and dash
-install_deps --and-package gcc-8-base
+install_deps --and-package $(find_pattern gcc-*-base)
 
 # note: these dependencies must be installed in a _very_ specific order and our dependency resolver
 # is not smart enough to handle that yet
 #install_deps --simultaneously dpkg
-DEBS="libattr1 libacl1 libgcc1 libc6 libpcre3 libselinux1 libzstd1 zlib1g liblzma5 libbz2-1.0"
+DEBS="libattr1 libacl1 libgcc1 libgcc-s1 libc6 libpcre2-* libpcre3 libselinux1 libzstd1 zlib1g liblzma5 libbz2-1.0 libgmp10"
 #DEBS="libattr1 libc6 libgcc1 libpcre3 libacl1 libc6 libselinux1   libbz2-1.0 liblzma5 zlib1g"
+DEBS=$(find_pattern ${DEBS})
 dpkg -i $(add_deb_path ${DEBS})
 
 # unpack the files from a few core packages so that we have what we need on the filesystem to switch
@@ -34,12 +46,16 @@ install_deps --and-package coreutils debianutils dpkg
 install_deps dash
 install_deps --and-package mawk dash
 
-# needed for "pager" in dpkg (dpkg -l):
-install_deps --and-package less
+# less is needed for "pager" in dpkg (dpkg -l) and libcrypt1 is needed for some versions of perl-base:
+install_deps --and-package \
+    less \
+    libcrypt1 \
+;
 
 # needed for actually installing anything with apt
 install_deps --and-package \
     libc-bin \
+    debconf \
     diffutils \
     findutils \
 ;

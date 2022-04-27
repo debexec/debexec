c84c3c9ec71bc0a08cf3c8202426ebf73336982f
Erich E. Hoover <erich.e.hoover@gmail.com>
2022-04-27 09:59:40 -0600

    allow loading an application-specific config file

--- load-config.sh
+++ load-config.sh
@@ -4,3 +4,8 @@ ARCHITECTURE="amd64"
 DISTRIBUTION="unstable"
 COMPONENTS="main non-free contrib"
 MIRRORSITE="http://deb.debian.org/debian" # latest and greatest
+
+DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+if [ -f "${DIR}"/debexecrc ]; then
+    . "${DIR}"/debexecrc
+fi

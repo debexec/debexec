6849d2b7afb18d341d93f7a670771ee7c03b0d29
Erich E. Hoover <erich.e.hoover@gmail.com>
2022-05-18 13:56:45 -0600

    copy the preloader into the container

--- scripts/config-preload.sh
+++ scripts/config-preload.sh
@@ -4,5 +4,6 @@ echo 'APT::Sandbox::Verify::IDs "false";' >> /etc/apt/apt.conf.d/99-sandbox
 
 # hook ownership routines such that common sudo operations will complete successfully
 if [ -f "${DEBEXEC_DIR}"/lib/debexec-preload.so ]; then
-    echo "${DEBEXEC_DIR}"/lib/debexec-preload.so > /etc/ld.so.preload
+    cp -a "${DEBEXEC_DIR}"/lib/debexec-preload.so /var/cache/debexec
+    echo /var/cache/debexec/debexec-preload.so > /etc/ld.so.preload
 fi

86fba2e484ac75473b307c84e2cef21ed4f2ae04
Erich E. Hoover <erich.e.hoover@gmail.com>
2022-05-24 15:19:55 -0600

    fix FIFO hang when launching without a GUI

--- scripts/launch-gui.sh
+++ scripts/launch-gui.sh
@@ -11,6 +11,8 @@ printf "DEBEXEC_LAUNCH=${DEBEXEC_LAUNCH}" > "${DEBEXEC_TOGUI}"
 . "${DEBEXEC_FROMGUI}"; export DEBEXEC_GUI # DEBEXEC_GUI=[0|1]
 if [ "${DEBEXEC_GUI}" -eq "0" ]; then
     cat "${DEBEXEC_APTFIFO}" >/dev/null 2>/dev/null &
+    rm "${DEBEXEC_TOGUI}" "${DEBEXEC_FROMGUI}" "${DEBEXEC_APTFIFO}"
+    touch "${DEBEXEC_TOGUI}" "${DEBEXEC_FROMGUI}" "${DEBEXEC_APTFIFO}"
 fi
 
 # do not ever ask for input from the user

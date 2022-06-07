4d38b5fbc0cce380283038784f13c2fb21c7f256
Erich E. Hoover <erich.e.hoover@gmail.com>
2022-06-06 21:59:45 -0600

    add support for using the host nVidia Vulkan driver inside the container

--- scripts/config-video.sh
+++ scripts/config-video.sh
@@ -1,17 +1,25 @@
+GLX_DIRS="\
+    /usr/lib/i386-linux-gnu \
+    /usr/lib/x86_64-linux-gnu \
+"
 GLX_LIBS="\
-    /usr/lib/i386-linux-gnu/libGLX_nvidia.so.0 \
-    /usr/lib/x86_64-linux-gnu/libGLX_nvidia.so.0 \
-";
-
-LIBS=""
-for GLX_LIB in ${GLX_LIBS}; do
-    DEPS=$(ldd "${GLX_LIB}" | sed -n 's|.* => \(.*libnvidia.*\) (.*)|\1|p')
-    LIBS="${LIBS} ${GLX_LIB} ${DEPS}"
+    libGLX_nvidia.so.0 \
+    libnvidia-glvkspirv.so.* \
+"
+FILES="\
+    /usr/share/vulkan/icd.d/nvidia_icd.json \
+    /usr/share/vulkan/implicit_layer.d/nvidia_layers.json \
+"
+for GLX_DIR  in ${GLX_DIRS}; do
+    for GLX_LIB in ${GLX_LIBS}; do
+        DEPS=$(ldd "${GLX_DIR}"/${GLX_LIB} | sed -n 's|.* => \(.*libnvidia.*\) (.*)|\1|p')
+        FILES="${FILES} ${GLX_DIR}/${GLX_LIB} ${DEPS}"
+    done
 done
-for LIB in ${LIBS}; do
-    HOST_MD5=$(md5sum "${LIB}" | sed 's/ .*//')
-    CONT_MD5=$(md5sum "${FAKEROOT}"/"${LIB}" | sed 's/ .*//')
+for FILE in ${FILES}; do
+    HOST_MD5=$(md5sum "${FILE}" 2>/dev/null | sed 's/ .*//')
+    CONT_MD5=$(md5sum "${FAKEROOT}"/"${FILE}" 2>/dev/null | sed 's/ .*//')
     if [ "${HOST_MD5}" != "${CONT_MD5}" ]; then
-        cp "${LIB}" "${FAKEROOT}"/"${LIB}"
+        cp "${FILE}" "${FAKEROOT}"/"${FILE}"
     fi
 done
